98649b52d3d036aac574dc01aab3ec8f7404b1d3
Pereloma <gerzhod@Gmail.com>
2020-11-27 14:24:17 +0700

    8 all isEnd

---
+++ src/homework8/task2/Main.java
@@ -0,0 +1,10 @@
+package homework8.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        MyInteger integer = new MyInteger(12345);
+        for (Integer i : integer){
+            System.out.println(i);
+        }
+    }
+}
