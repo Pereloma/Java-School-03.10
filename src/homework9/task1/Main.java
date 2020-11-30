9f031e70a4415e1eaf9c172eb376c6d630cbd51b
Pereloma <gerzhod@Gmail.com>
2020-11-30 10:18:12 +0700

    9 isStart

---
+++ src/homework9/task1/Main.java
@@ -0,0 +1,41 @@
+package homework9.task1;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Comparator<Integer> integerComparator = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if(getPrimeDivisors(o1)>getPrimeDivisors(o2))
+                    return -1;
+                else return 1;
+            }
+        };
+        List<Integer> integers = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            integers.add((int)(Math.random()*99d)+1);
+        }
+
+        for (Integer integer : integers){
+            System.out.println(integer);
+        }
+        System.out.println();
+        integers.sort(integerComparator);
+
+        for (Integer integer : integers){
+            System.out.println(integer);
+        }
+    }
+    private static int getPrimeDivisors(int value){
+        Set<Integer> divisors = new HashSet<>();
+        divisors.add(value);
+
+        for (int i = value-1; i > 0; i--) {
+            if(value%i == 0)
+                divisors.add(i);
+        }
+
+        return divisors.size();
+    }
+}
